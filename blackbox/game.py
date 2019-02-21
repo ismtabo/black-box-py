1406eba0f41e64af84c7ba30e70fc5c91bb92b90
angmore <angelmorenocalvo@gmail.com>
2019-02-21 09:05:54 +0100

    new funtion to add the mirors

--- blackbox/game.py
+++ blackbox/game.py
@@ -1,13 +1,28 @@
 from .table import Table
-
+from random import randint
 # var size_x and size_y are determinate of the size of the table
 
 class Game:
+    # recive 3 parameters, the lenght, the whidth, and the number of mirors
     def __init__(self, size_x, size_y, mirors):
-        self.__mirors = mirors
+        self._mirors = mirors
         self._size_x = size_x
         self._size_y = size_y
-        self.table = Table(size_x,size_y,mirors)
+        self.table = Table(size_x,size_y)
 
+
+    # Provisional process to see the table how it works
     def print_table(self):
         self.table.print_table()
+
+
+    def put_mirors(self):
+        i = 0
+        while i < self._mirors:
+            coordenate_x = randint(0, self._size_x - 1)
+            coordenate_y = randint(0, self._size_y - 1)
+            if self.table.get_element(coordenate_y,coordenate_x) != '*':
+                self.table.add_element(coordenate_y,coordenate_x, '*')
+                i+=1
+            else:
+                pass
--- blackbox/table.py
+++ blackbox/table.py
@@ -1,15 +1,16 @@
 class Table:
-    def __init__(self, size_x, size_y,mirors):
+        # recive 2 parameters, the lenght and the whidth
+    def __init__(self, size_x, size_y):
         self._size_x = size_x
         self._size_y = size_y
-        self.mirors = mirors
         self.p = 0
 
 
-
+    # generate the table 
     def create_table(self):
         self.p=[[0 for x in range(self._size_y)]for x in range(self._size_x)]
 
+    # provisional process that make easy to see how works
     def print_table(self):
 
         for i in self.p:
@@ -17,13 +18,17 @@ class Table:
                     print(i)
         print("\n")
 
+    # Process whit you can include an element on a part of the table
     def add_element(self,coordenate_y, coordenate_x, value):
         self.p[coordenate_y][coordenate_x] = value
-
-
+    # obtein the element inside the coordenates (y,x)
+    def get_element(self, coordenate_y, coordenate_x):
+        return self.p[coordenate_y][coordenate_x]
+    # function with can know if the coordenate are in the table
+    # movement is a list where there are firt de coordenate y and then the coordenate x
     def in_table(self, movement):
-        coordenate_x = movement[1]
-        coordenate_y = movement[0]
+        coordenate_x = movement[1] - 1
+        coordenate_y = movement[0] - 1 
         if (coordenate_x > self._size_x) or (coordenate_x < 0):
             raise Exception(" Coordenate X out of range")
         
